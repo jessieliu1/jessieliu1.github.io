f45bc53c4f74d012a9fb477e8b59cd9813bed100
jessieliu1 <jliu997@gmail.com>
2026-07-21 22:59:33 -0700

    hide home link when there are no posts.

--- src/utils/menu.ts
+++ src/utils/menu.ts
@@ -2,9 +2,13 @@ import { menuLinks } from "@/site-config";
 
 import { getAllPosts } from "./post";
 
-/** Menu links, minus the blog link when there are no published posts. */
+/**
+ * Menu links, minus the blog link when there are no published posts. With no
+ * blog to link to, Home just duplicates the site title, so it goes too and the
+ * menu renders empty.
+ */
 export async function getVisibleMenuLinks() {
 	const posts = await getAllPosts();
 	if (posts.length > 0) return menuLinks;
-	return menuLinks.filter((link) => link.path !== "/posts/");
+	return menuLinks.filter((link) => link.path !== "/posts/" && link.path !== "/");
 }
